02e60b1c8372e74f8f5c066d6dbc246011ba958c
ceptre aka kyroshie aka rzekomo niejaki <brandeburglukasz@gmail.com>
2024-03-07 11:31:37 +0100

    Fixed Camera Shake being different on every framerate

--- Picklespin/Assets/Scripts/Player/CameraShake.cs
+++ Picklespin/Assets/Scripts/Player/CameraShake.cs
@@ -8,10 +8,14 @@ public class CameraShake : MonoBehaviour
     
     public float landShakeStrenght;
 
+    private float fpsCompensate;
+
+
 
     public void ShootCameraShake()
     {
-        mainCamera.DOShakeRotation(0.15f, 1.2f, 26, 90, true, ShakeRandomnessMode.Harmonic);
+        MakeShakeEqualOnEveryFPS();
+        mainCamera.DOShakeRotation(0.17f, 1.2f * fpsCompensate, 26, 90, true, ShakeRandomnessMode.Harmonic);
         hand.DOShakePosition(0.2f, 0.1f, 40, 90, false, true, ShakeRandomnessMode.Harmonic);
     }
 
@@ -19,7 +23,8 @@ public class CameraShake : MonoBehaviour
 
     public void LandCameraShake()
     {
-        mainCamera.DOShakeRotation(landShakeStrenght*0.05f + 0.05f, 0.1f * landShakeStrenght, 26, 90, true, ShakeRandomnessMode.Harmonic);
+        MakeShakeEqualOnEveryFPS();
+        mainCamera.DOShakeRotation(landShakeStrenght*0.05f + 0.05f, 0.1f * landShakeStrenght * fpsCompensate, 26, 90, true, ShakeRandomnessMode.Harmonic);
         hand.DOShakePosition(0.15f, 0.05f * landShakeStrenght, 40, 90, false, true, ShakeRandomnessMode.Harmonic);
     }
 
@@ -27,13 +32,22 @@ public class CameraShake : MonoBehaviour
 
     public void ExplosionNearbyShake(float distanceFromExplosion) //maybe do a delay based on distance, so wait until the shockwave of an explosion gets to you
     {
+        MakeShakeEqualOnEveryFPS();
+
         distanceFromExplosion = Mathf.Clamp(distanceFromExplosion, 5, 45);
 
-        float strenghtBasedOnDistance = (45 - distanceFromExplosion) * 0.01f;
+        float strenghtBasedOnDistance = (45 - distanceFromExplosion) * 0.015f;
 
 
-        mainCamera.DOShakeRotation(strenghtBasedOnDistance, strenghtBasedOnDistance, 26, 90, true, ShakeRandomnessMode.Harmonic);
-        hand.DOShakePosition(strenghtBasedOnDistance, strenghtBasedOnDistance * 0.02f, 40, 90, false, true, ShakeRandomnessMode.Harmonic);
+        mainCamera.DOShakeRotation(strenghtBasedOnDistance * fpsCompensate, strenghtBasedOnDistance * (fpsCompensate * 2), 26, 90, true, ShakeRandomnessMode.Harmonic);
+        hand.DOShakePosition(strenghtBasedOnDistance * fpsCompensate, strenghtBasedOnDistance * (fpsCompensate * 2) * 0.02f, 40, 90, false, true, ShakeRandomnessMode.Harmonic);
+    }
+
+
+    private void MakeShakeEqualOnEveryFPS()
+    {
+        fpsCompensate = 1/(Time.smoothDeltaTime * 59.5f);
+        Debug.Log(fpsCompensate);
     }
 
 
