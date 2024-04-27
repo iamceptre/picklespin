bc3f275fc47740d9d6177a44fa267f24e67f7ff2
ceptre aka kyroshie aka rzekomo niejaki <brandeburglukasz@gmail.com>
2024-04-27 17:34:05 +0200

    gui tweaks
    countdown bar will now go red past 50%

--- Picklespin/Assets/EscapeTimer.cs
+++ Picklespin/Assets/EscapeTimer.cs
@@ -4,7 +4,12 @@ using UnityEngine.UI;
 
 public class EscapeTimer : MonoBehaviour
 {
+    private Color sexyRed = new Color(0.76f, 0.235f, 0.235f);
+    private Color startingColor;
+    private bool startedColoring = false;
+
     [SerializeField] private Slider mySlider;
+    [SerializeField] private Image sliderFill;
     private Death death;
 
     [SerializeField] private float countdownSpeed = 1;
@@ -36,6 +41,8 @@ public class EscapeTimer : MonoBehaviour
 
         death = Death.instance;
 
+        startingColor = sliderFill.color;
+
         if (enableWithMe.Length > 0)
         {
             EnableOtherObjects();
@@ -52,12 +59,30 @@ public class EscapeTimer : MonoBehaviour
         {
             mySlider.value -= Time.deltaTime * countdownSpeed;
 
+            if(mySlider.value <= 0.5f && !startedColoring)
+            {
+                startedColoring = true;
+                var colorRoutine = StartCoroutine(ColorTheFill());
+            }
+
             if (mySlider.value <= 0)
             {
+                StopAllCoroutines();
                 death.PlayerDeath();
                 enabled = false;
             }
 
+            yield return null;
+        }
+    }
+
+
+    private IEnumerator ColorTheFill()
+    {
+        while (true)
+        {
+            var t = 0.5f / mySlider.value - 1;
+            sliderFill.color = Color.Lerp(startingColor, sexyRed, t);
             yield return null;
         }
     }
