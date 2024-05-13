8aad8a40148c4888cf36d1d0dc2bc81ee34e1ef2
ceptre aka kyroshie aka rzekomo niejaki <brandeburglukasz@gmail.com>
2024-05-13 23:22:54 +0200

    minor update
    adjusted bhop values
    adjusted ui bars

--- Picklespin/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Picklespin/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -287,7 +287,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetAxis("Horizontal") != 0)
         {
-            externalPushForce = 0.5f + speedometer.horizontalVelocity * 0.25f;
+            externalPushForce = 0.5f + speedometer.horizontalVelocity * 0.32f;
             StopAllCoroutines();
             StartCoroutine(ExternalPushForceDamp());
         }
