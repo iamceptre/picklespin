2bebaae621f4c2d8b9ea10cc0fdf0514d3b97938
ceptre aka kyroshie aka rzekomo niejaki <brandeburglukasz@gmail.com>
2024-05-14 22:33:49 +0200

    improved portal
    added fmod snapshot effect when portal closes
    improved portal visuals and post-closage behaviour

--- Picklespin/Assets/Scripts/LightPulsing.cs
+++ Picklespin/Assets/Scripts/LightPulsing.cs
@@ -3,21 +3,28 @@ using UnityEngine;
 
 public class LightPulse : MonoBehaviour
 {
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private float lowestIntensity = 0;
+    [SerializeField] private float animationTime = 1f;
+    [SerializeField] private float lowestIntensity;
 
     private Light lightSource;
-    private float lightIntensity;
+    private float startLightIntensity;
 
     private void Awake()
     {
         lightSource = GetComponent<Light>();
-        lightIntensity = lightSource.intensity;
+        startLightIntensity = lightSource.intensity;
     }
     void Start()
     {
         lightSource = GetComponent<Light>();
-        DOTween.To(() => lightSource.intensity, x => lightSource.intensity = x, lowestIntensity, speed).SetLoops(-1, LoopType.Yoyo);
+        lightSource.intensity = 0;
+        lightSource.DOIntensity(startLightIntensity, 0.5f).OnComplete(Pulse);
     }
+
+    private void Pulse()
+    {
+        lightSource.DOIntensity(lowestIntensity, animationTime).SetLoops(-1, LoopType.Yoyo);
+    }
+
 
 }
