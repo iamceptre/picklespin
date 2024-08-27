ed08777f9a22612ab1f1cdcb715795a028c69822
ceptre aka kyroshie aka rzekomo niejaki <brandeburglukasz@gmail.com>
2024-08-27 22:09:54 +0200

    cleaned up MoveToAnotherCanvas script

--- Picklespin/Assets/Scripts/MoveToAnotherCanvas.cs
+++ Picklespin/Assets/Scripts/MoveToAnotherCanvas.cs
@@ -5,65 +5,46 @@ using System.Collections;
 
 public class MoveToAnotherCanvas : MonoBehaviour
 {
-    [Tooltip("Fade out those")]
-    private Canvas canvasToGoFrom;
-    [Tooltip("Fade in those")]
+    [Tooltip("Canvas to fade out, aka me")]
+    [SerializeField] private Canvas canvasToGoFrom;
+
+    [Tooltip("Canvas to fade in")]
     [SerializeField] private Canvas canvasToGoTo;
 
     private CanvasGroup canvasGroupFrom;
     private CanvasGroup canvasGroupTo;
 
-    private bool isTransitioning = false; 
-
     private void Awake()
     {
-        canvasToGoFrom = GetComponentInParent<Canvas>();
-        canvasGroupFrom = canvasToGoFrom?.gameObject.GetComponent<CanvasGroup>();
-        canvasGroupTo = canvasToGoTo?.gameObject.GetComponent<CanvasGroup>();
+        canvasGroupFrom = canvasToGoFrom.GetComponent<CanvasGroup>();
+        canvasGroupTo = canvasToGoTo.GetComponent<CanvasGroup>();
     }
 
     public void Do()
     {
-        if (isTransitioning) return; 
-        StartCoroutine(FadeOutCoroutine());
+        StartCoroutine(TransitionCoroutine());
     }
 
-    private IEnumerator FadeOutCoroutine()
+    private IEnumerator TransitionCoroutine()
     {
-        isTransitioning = true;
-
-        if (canvasGroupFrom == null || canvasGroupTo == null)
-        {
-            Debug.LogError("CanvasGroup is missing on one of the canvases.");
-            isTransitioning = false;
-            yield break;
-        }
-
         canvasGroupFrom.interactable = false;
         canvasGroupFrom.blocksRaycasts = false;
 
-        RuntimeManager.PlayOneShot("event:/UI/UI_WHISPER");
 
-        canvasGroupFrom.DOKill(true);
-        canvasGroupTo.DOKill(true);
-
+        canvasGroupFrom.DOKill();
         canvasGroupFrom.alpha = 1;
         yield return canvasGroupFrom.DOFade(0, 0.1f).SetEase(Ease.InOutSine).WaitForCompletion();
 
         canvasToGoFrom.enabled = false;
+        RuntimeManager.PlayOneShot("event:/UI/UI_WHISPER");
 
-        StartCoroutine(FadeInCoroutine());
-    }
-
-    private IEnumerator FadeInCoroutine()
-    {
         canvasToGoTo.enabled = true;
 
+        canvasGroupTo.DOKill();
         canvasGroupTo.alpha = 0;
         yield return canvasGroupTo.DOFade(1, 0.16f).SetEase(Ease.InOutSine).WaitForCompletion();
 
         canvasGroupTo.interactable = true;
         canvasGroupTo.blocksRaycasts = true;
-        isTransitioning = false;
     }
 }
